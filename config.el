cc493d09567634f33ad50701828a96e942821f0b
kostia <viocost@gmail.com>
2020-02-12 13:08:36 +0500

    Transparency default added

--- config.el
+++ config.el
@@ -69,4 +69,11 @@
 
 (setq org-log-done 'time)
 
+;; transparency
+(defun transparency (value)
+   "Sets the transparency of the frame window. 0=transparent/100=opaque"
+   (interactive "nTransparency Value 0 - 100 opaque:")
+   (set-frame-parameter (selected-frame) 'alpha value))
 
+;; setting default transaprency to 85
+(transparency 85)

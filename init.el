787aefefb60b1dd4dd06a5cd4adc0664eb6fc542
kostia <viocost@gmail.com>
2020-06-24 15:59:37 +0500

    Added haskell and go

--- init.el
+++ init.el
@@ -122,8 +122,8 @@
        ;;faust             ; dsp, but you get to keep your soul
        ;;fsharp           ; ML stands for Microsoft's Language
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
-       ;;go                ; the hipster dialect
-       ;;(haskell +dante)  ; a language that's lazier than I am
+       go                ; the hipster dialect
+       (haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
